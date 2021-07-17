c0fffa6cfb58eeb657efc2606ad4e182eb1e2e1a
Rafael Framil <raframil@gmail.com>
2021-07-17 10:45:08 -0300

    chore: start project

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'nativescript-vue'
+import App from './components/App'
+
+
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = (TNS_ENV === 'production')
+
+Vue.registerElement(
+  'RadSideDrawer',
+  () => require('nativescript-ui-sidedrawer').RadSideDrawer
+)
+
+new Vue({
+  
+  render: h => h('frame', [h(App)])
+}).$start()
